6b893f7f496135a35c88b6532add0770a2ff44a8
vcandappa <vcandapp@redhat.com>
2022-03-02 19:41:26 +0530

    New tht files added

--- 17-dev-lab-scripts/osp/scripts_v2/install_configure.sh
+++ 17-dev-lab-scripts/osp/scripts_v2/install_configure.sh
@@ -7,6 +7,7 @@ TMPL_DIR="osp${release%.*}_ref"
 CMD_FILE="overcloud_deploy_${deploy_type}.sh"
 COMMON_NET_DATA="17-dev-lab-scripts/osp/network_data_v2/${server}"
 COMMON_NET_DATA_V2="17-dev-lab-scripts/osp/network_data_v2/${server}_v2"
+COMMON_VIP_DATA="17-dev-lab-scripts/osp/network_data_v2/vip_data.yaml"
 
 THT_BASE=`basename $THT_URL`
 THT_DIR="${THT_BASE%.git}"
@@ -35,6 +36,7 @@ scp ${OPT} network_data.yaml root@${server}:/root/infrared/
 scp ${OPT} network_data.yaml root@${server}:/root/infrared/${THT_PATH}/
 cp $COMMON_NET_DATA_V2 ${THT_PATH}/network_data_v2.yaml
 scp ${OPT} ${THT_PATH}/network_data_v2.yaml root@${server}:/root/infrared/${THT_PATH}/
+scp ${OPT} $COMMON_VIP_DATA root@${server}:/root/infrared/${THT_PATH}/
 
 awk -v var="$VLAN_CONFIG" 'BEGIN{x=var;FS="\\n"} /NeutronNetworkVLANRanges:/{gsub("NeutronNetworkVLANRanges:.*","NeutronNetworkVLANRanges: dpdk1:"x+5":"x+10",dpdk2:"x+5":"x+10",sriov1:"x+5":"x+10",sriov2:"x+5":"x+10   )} {print}' ${THT_PATH}/network-environment.yaml > network-environment.yaml
 scp ${OPT} network-environment.yaml root@${server}:/root/infrared/${THT_PATH}
