a282e8794b62f5cbff7314b7176a6c85d46e13f0
vcandappa <vcandapp@redhat.com>
2022-12-02 23:59:01 +0530

    Add stage for overcloud

--- osnet_nmstate_osp17/osp/scripts_v2/install_overcloud_remote.sh
+++ osnet_nmstate_osp17/osp/scripts_v2/install_overcloud_remote.sh
@@ -1,52 +1,40 @@
 #!/bin/bash
-###########################################################
-# This script runs on the hypervisor node ################
-###########################################################
 
-set -ex
+PARAMS="$*"
+USER_THT="$HOME/osp17_ref"
 
-if [ "$#" -ne 2 ]; then
-    echo "ERROR: Invalid Arguments"
-    exit 1
+if [ ! -d /home/stack/images ]; then
+    mkdir -p /home/stack/images
+    pushd /home/stack/images
+    for i in /usr/share/rhosp-director-images/overcloud-hardened-uefi-full.tar  /usr/share/rhosp-director-images/ironic-python-agent-latest.tar; do tar -xvf $i; done
+    sudo yum install libguestfs-tools -y
+    export LIBGUESTFS_BACKEND=direct
+    virt-customize --root-password password:redhat -a overcloud-hardened-uefi-full.qcow2
+    openstack overcloud image upload --os-image-name overcloud-hardened-uefi-full.qcow2 --update-existing
+    for i in $(openstack baremetal node list -c UUID -f value); do openstack overcloud node configure --boot-mode "uefi" $i; done
+    popd
 fi
 
-RELEASE=$1
-THT_PATH=$2
+#mkdir -p mount_image
+#guestmount -a overcloud-hardened-uefi-full.raw -i --rw mount_image
+#cd ..
+#guestunmount mount_image
+#exit
 
-#repo_url='http://github.com/rh-nfv-int/osp-dev-lab-tht.git'
-#THT_URL="${repo_url}"
-#TMPL_DIR="osp${release%.*}_ref"
-
-#THT_BASE=`basename $THT_URL`
-#THT_DIR="${THT_BASE%.git}"
-
-cd /root/infrared
-source .venv/bin/activate
-
-UNDER_SUBNET=$(cat undercloud.conf |grep ^local_subnet|awk 'BEGIN{FS=OFS="="} {print $2}'|sed 's/ //'g)
-if [ -z $UNDER_SUBNET ]; then
-  UNDER_SUBNET=br-ctlplane
-fi
-
-if [[ ${RELEASE} == "13" ]]; then
-    NET_BACKEND=' --network-ovn no --network-ovs yes --network-backend vxlan '
-else
-    NET_BACKEND=' --network-ovn yes --network-ovs no --network-backend geneve '
-fi
-
-CONTAINER= --containers True
-
-infrared tripleo-overcloud -vv \
-    --version ${RELEASE} \
-    --introspect=no --tagging=no --tht-roles yes --deploy=yes --overcloud-templates none \
-    ${NET_BACKEND} --network-protocol ipv4 \
-    --overcloud-ssl yes  --vbmc-host hypervisor \
-    -e provison_virsh_network_name=br-ctlplane \
-    --deployment-files ${THT_PATH} \
-    --overcloud-debug yes \
-    --hybrid instack.json  --overcloud-script  /root/infrared/overcloud_deploy.sh
+#virt-customize -a overcloud-hardened-uefi-full.qcow2 --upload nmstate/impl_nmstate.py:/usr/lib/python3.9/site-packages/os_net_config/impl_nmstate.py
+#vi nmstate/cli.py
+#virt-customize -a overcloud-hardened-uefi-full.qcow2 --upload nmstate/cli.py:/usr/lib/python3.9/site-packages/os_net_config/cli.py
+#vi nmstate/objects.py
+#virt-customize -a overcloud-hardened-uefi-full.qcow2 --upload nmstate/objects.py:/usr/lib/python3.9/site-packages/os_net_config/objects.py
 
 
-#-e provison_virsh_network_name=${UNDER_SUBNET} \
-#infrared tripleo-overcloud -o overcloud-install.yml
-#--vbmc-force False --vbmc-host undercloud --tagging no --deploy no --boot-mode "bios"
+echo "Pre-provision node...."
+
+openstack overcloud node provision \
+  --stack overcloud \
+  --timeout 120  \
+  --network-config -y \
+  --templates /usr/share/openstack-tripleo-heat-templates \
+  --working-dir /home/stack/overcloud_deploy_17 \
+  --output $HOME/templates/overcloud-baremetal-deployed.yaml \
+  $USER_THT/network/baremetal_deployment_nm.yaml
