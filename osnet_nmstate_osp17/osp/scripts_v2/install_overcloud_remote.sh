1ca6e7f6e7e3ef7070106a1baa12024be19c695e
vcandappa <vcandapp@redhat.com>
2022-12-06 00:17:05 +0530

    Update stage

--- osnet_nmstate_osp17/osp/scripts_v2/install_overcloud_remote.sh
+++ osnet_nmstate_osp17/osp/scripts_v2/install_overcloud_remote.sh
@@ -3,18 +3,6 @@
 PARAMS="$*"
 USER_THT="$HOME/osp17_ref"
 
-if [ ! -d /home/stack/images ]; then
-    mkdir -p /home/stack/images
-    pushd /home/stack/images
-    for i in /usr/share/rhosp-director-images/overcloud-hardened-uefi-full.tar  /usr/share/rhosp-director-images/ironic-python-agent-latest.tar; do tar -xvf $i; done
-    sudo yum install libguestfs-tools -y
-    export LIBGUESTFS_BACKEND=direct
-    virt-customize --root-password password:redhat -a overcloud-hardened-uefi-full.qcow2
-    openstack overcloud image upload --os-image-name overcloud-hardened-uefi-full.qcow2 --update-existing
-    for i in $(openstack baremetal node list -c UUID -f value); do openstack overcloud node configure --boot-mode "uefi" $i; done
-    popd
-fi
-
 #mkdir -p mount_image
 #guestmount -a overcloud-hardened-uefi-full.raw -i --rw mount_image
 #cd ..
@@ -30,11 +18,14 @@ fi
 
 echo "Pre-provision node...."
 
-openstack overcloud node provision \
-  --stack overcloud \
-  --timeout 120  \
-  --network-config -y \
-  --templates /usr/share/openstack-tripleo-heat-templates \
-  --working-dir /home/stack/overcloud_deploy_17 \
-  --output $HOME/templates/overcloud-baremetal-deployed.yaml \
-  $USER_THT/network/baremetal_deployment_nm.yaml
+infrared tripleo-overcloud -vv --version 
+    --version $RELEASE \
+    --introspect=no --tagging=no \
+    --tht-roles yes --deploy=yes \
+    --overcloud-templates none --network-ovn yes \
+    --network-ovs no --network-backend geneve \
+    --network-protocol ipv4 --overcloud-ssl yes \
+    --deployment-files osp-dev-lab-tht/osp17_ref \
+    -e provison_virsh_network_name=ctlplane-subnet \
+    --hybrid instack.json \
+    --overcloud-script /root/infrared/overcloud_deploy.sh
