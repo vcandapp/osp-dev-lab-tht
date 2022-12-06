4fb10f3115fe2246c270defb8088d3e1792660dc
vcandappa <vcandapp@redhat.com>
2022-12-07 01:06:35 +0530

    Testing

--- osnet_nmstate_osp17/osp/scripts_v2/install_overcloud_remote.sh
+++ osnet_nmstate_osp17/osp/scripts_v2/install_overcloud_remote.sh
@@ -22,5 +22,5 @@ echo "Deploying OSP version ${RELEASE}"
 cd /root/infrared/
 source .venv/bin/activate
 
-infrared tripleo-overcloud -o overcloud-install.yml --version 17.1 --deployment-files /root/infrared/osnet_nm_dev_auto_tht/osp17_ref  --overcloud-templates none --storage-backend lvm --overcloud-ssl no --network-backend vxlan --network-protocol ipv4 --network-bgpvpn no  --network-dvr no  --network-l2gw no --storage-external no --splitstack no --overcloud-debug yes  --overcloud-fencing no  --introspect no  --tagging no --tht-roles yes --deploy yes  --containers True --hybrid instack.json -e provison_virsh_network_name=br-ctlplane  --overcloud-script /root/infrared/overcloud_deploy.sh --collect-ansible-facts False --ntp-pool clock.corp.redhat.com
+infrared tripleo-overcloud -o overcloud-install.yml --version 17.1 --deployment-files osnet_nm_dev_auto_tht/osp17_ref  --overcloud-templates none --storage-backend lvm --overcloud-ssl no --network-backend vxlan --network-protocol ipv4 --network-bgpvpn no  --network-dvr no  --network-l2gw no --storage-external no --splitstack no --overcloud-debug yes  --overcloud-fencing no  --introspect no  --tagging no --tht-roles yes --deploy yes  --containers True --hybrid instack.json -e provison_virsh_network_name=br-ctlplane  --overcloud-script overcloud_deploy.sh --collect-ansible-facts False --ntp-pool clock.corp.redhat.com
 
