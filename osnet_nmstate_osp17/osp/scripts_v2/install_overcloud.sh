fd78b2b68d173535a9370a5265aef17ad2ed1423
vcandappa <vcandapp@redhat.com>
2022-12-06 21:59:20 +0530

    Fix err

--- osnet_nmstate_osp17/osp/scripts_v2/install_overcloud.sh
+++ osnet_nmstate_osp17/osp/scripts_v2/install_overcloud.sh
@@ -7,16 +7,16 @@ repo_url='https://github.com/vcandapp/osp-dev-lab-tht.git'
 THT_URL="${repo_url}"
 TMPL_DIR="osp${release%.*}_ref"
 
-if [ -d $THT_DIR]; then
-    rm -rf $THT_DIR
-fi
-
 # Cloning to jenkins workspace folder
 git clone --depth=1 $THT_URL
 
 THT_BASE=`basename $THT_URL`
 THT_DIR="${THT_BASE%.git}"
 THT_PATH="${THT_DIR}/${TMPL_DIR}"
+
+if [ -d $THT_DIR]; then
+    rm -rf $THT_DIR
+fi
 
 OPT="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
