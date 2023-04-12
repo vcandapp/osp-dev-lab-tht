09103ef49618a059618d3ce624051cef6cb04eda
vcandappa <vcandapp@redhat.com>
2023-04-12 14:37:21 +0530

    Updating CI scripts

--- osnet_nmstate_osp17/osp/scripts_v2/install_configure.sh
+++ osnet_nmstate_osp17/osp/scripts_v2/install_configure.sh
@@ -15,7 +15,7 @@ THT_BASE=`basename $THT_URL`
 THT_DIR="${THT_BASE%.git}"
 THT_PATH="${THT_DIR}/${TMPL_DIR}"
 
-if [ -d $THT_DIR]; then
+if [ -d $THT_DIR ]; then
     rm -rf $THT_DIR
 fi
 # Cloning to jenkins workspace folder
