b92b3f2041a8a300f53892f13d394306b18681d9
vcandapp <vcandapp@redhat.com>
2024-12-04 17:36:16 +0530

    Scripts for osnet ci pipeline

--- osnet_nmstate_osp17/osp/scripts_v2/osnetconfig_devci.sh
+++ osnet_nmstate_osp17/osp/scripts_v2/osnetconfig_devci.sh
@@ -4,7 +4,7 @@ set -ex
 
 OPT="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
-scp $OPT osnet_nmstate_osp17/osp/scripts_v2/osnetconfig_devci_remote root@${server}:/root/
-CMD="bash /root/osnetconfig_devci_remote"
+scp $OPT osnet_nmstate_osp17/osp/scripts_v2/osnetconfig_devci_remote.sh root@${server}:/root/
+CMD="bash /root/osnetconfig_devci_remote.sh"
 ssh $OPT root@${server} "echo ${CMD}>>/root/auto-cmd-history"
 ssh $OPT root@${server} ${CMD}
